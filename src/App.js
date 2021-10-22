f9527e4646a3f71d133ef341f14f809a10383219
elham136999 <elhamkorivand@yahoo.com>
2021-10-22 18:36:05 +0330

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,60 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+import {
+  Home,
+  Products,
+  SingleProduct,
+  About,
+  Cart,
+  Error,
+  Checkout,
+  PrivateRoute,
+} from "./pages";
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+      </Switch>
+      <Switch>
+        <Route exact path='/products'>
+          <Products />
+        </Route>
+      </Switch>
+      <Switch>
+        <Route exact path='/products/:id'>
+          <SingleProduct />
+        </Route>
+      </Switch>
+      <Switch>
+        <Route exact path='/about'>
+          <About />
+        </Route>
+      </Switch>
+      <Switch>
+        <Route exact path='/cart'>
+          <Cart />
+        </Route>
+      </Switch>
+      <Switch>
+        <Route exact path='/checkout'>
+          <Checkout />
+        </Route>
+      </Switch>
+      <Switch>
+        <Route exact path='*'>
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
